5c5a28d4eeac61f442e3b5ad28ada0f53849430a
Valeriia Lysenko <Valeriia.Lysenko@Harpercollins.com>
2022-12-14 18:02:08 +0200

    add beforeEach

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,10 +6,9 @@ import { AppService, DataResponse } from './app.service';
 import { Data } from './mock-data';
 
 describe('AppComponent', () => {
-  // let appServiceSpy = jasmine.createSpyObj('AppService', ['getData']);;
-
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
+  let appService: AppService;
   const data:  DataResponse[] = Data;
 
   beforeEach(async () => {
@@ -25,31 +24,31 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  beforeEach(async () => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+  });
+
+  beforeEach(async () => {
+    appService = fixture.debugElement.injector.get(AppService);
+  });
+
+  it('should create the app', () => {
     expect(component).toBeTruthy();
   });
 
   it(`should have as title 'unit-test-practice'`, () => {
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
     expect(component.title).toEqual('unit-test-practice');
   });
 
   it('should h1 contain Hello', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     const h1 = compiled.querySelector('h1');
     expect(h1?.textContent).toContain('Hello');
   });
 
   it('getData should be called during initialization', async () => {
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
-    const appService = fixture.debugElement.injector.get(AppService);
-    let spy = spyOn(appService, "getData").and.returnValue(
+    spyOn(appService, "getData").and.returnValue(
       of(data)
     );
     fixture.detectChanges();
